d4adcbee3723e11a9c2c40254672672dda4c0a72
GiaKhuong32 <khuonghoanggia3202@gmail.com>
2024-02-19 11:33:52 +0700

    baimoi

--- src/app/pages/layout/tabs/role-management/role-management.component.ts
+++ src/app/pages/layout/tabs/role-management/role-management.component.ts
@@ -1,11 +1,256 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnInit, TemplateRef, ViewChild} from '@angular/core';
 import { SharedModule } from '../../../../shared/shared.module';
+import {IconService, ListItem, TableHeaderItem, TableItem, TableModel} from 'carbon-components-angular';
+import Filter20 from '@carbon/icons/es/filter/20';
+import TrashCan20 from '@carbon/icons/es/trash-can/20';
+import Edit20 from '@carbon/icons/es/edit/20';
+import Close20 from '@carbon/icons/es/close/20';
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-role-management',
   standalone: true,
-  imports: [SharedModule],
+  imports: [SharedModule, NgClass],
   templateUrl: './role-management.component.html',
   styleUrl: './role-management.component.scss',
 })
-export class RoleManagementComponent {}
+export class RoleManagementComponent implements OnInit{
+  @Input() model = new TableModel();
+  disabled = false;
+  @Input() size = "md";
+  @Input() showSelectionColumn = false;
+  @Input() enableSingleSelect = false;
+  @Input() striped = false;
+  @Input() sortable = true;
+  @Input() isDataGrid = false;
+  @Input() noData = false;
+  @Input() stickyHeader = false;
+  @Input() skeleton = false;
+  @Input() ariaLabelledby = "table";
+  @Input() ariaDescribedby = "desc";
+  @Input() invalid = false;
+  @Input() placeholder = "Content";
+  @Input() invalidText = "";
+  @Input() dropUp = false;
+  @Input() warn = false;
+  @Input() theme = "dark";
+  @Input() warnText = "This is a warning";
+  listItems = [
+    <ListItem>{content: "User", selected: false},
+    <ListItem>{content: "Admin", selected: false},
+  ];
+
+  @ViewChild("overflowMenuItemTemplate", {static: false})
+  protected overflowMenuItemTemplate: TemplateRef<any> | undefined;
+  @ViewChild("overflowMenuItemTemplateRemove", {static: false})
+  protected overflowMenuItemTemplateRemove: TemplateRef<any> | undefined;
+  @ViewChild("overflowMenuItemTemplateEdit", {static: false})
+  protected overflowMenuItemTemplateEdit: TemplateRef<any> | undefined;
+
+  dataSet =  [];
+  selectPage(page: number) {
+    this.model.currentPage = page;
+  }
+
+  constructor(protected iconService: IconService) {
+
+  }
+
+  ngOnInit(): void {
+    this.iconService.registerAll([Filter20,TrashCan20,Edit20,Close20]);
+  }
+
+  ngOnChanges(): void {
+
+  }
+
+  ngAfterViewInit() {
+    let dataset = [
+      [
+        new TableItem({data: " Content"}),
+        new TableItem({data: "Content"}),
+        new TableItem({data: "Content"}),
+        new TableItem({
+          data: {id: "1"},
+          template: this.overflowMenuItemTemplate,
+        }), new TableItem({
+        data: {id: "1"},
+        template: this.overflowMenuItemTemplateRemove,
+      }),
+        new TableItem({
+          data: {id: "1"},
+          template: this.overflowMenuItemTemplateEdit,
+        }),
+      ],  [
+        new TableItem({data: " Content"}),
+        new TableItem({data: "Content"}),
+        new TableItem({data: "Content"}),
+        new TableItem({
+          data: {id: "2"},
+          template: this.overflowMenuItemTemplate,
+        }), new TableItem({
+          data: {id: "2"},
+          template: this.overflowMenuItemTemplateRemove,
+        }),
+        new TableItem({
+          data: {id: "2"},
+          template: this.overflowMenuItemTemplateEdit,
+        }),
+      ],  [
+        new TableItem({data: " Content"}),
+        new TableItem({data: "Content"}),
+        new TableItem({data: "Content"}),
+        new TableItem({
+          data: {id: "3"},
+          template: this.overflowMenuItemTemplate,
+        }), new TableItem({
+          data: {id: "3"},
+          template: this.overflowMenuItemTemplateRemove,
+        }),
+        new TableItem({
+          data: {id: "3"},
+          template: this.overflowMenuItemTemplateEdit,
+        }),
+      ],
+      [
+        new TableItem({data: "Content"}),
+        new TableItem({data: "Content"}),
+        new TableItem({data: "Content"}),
+        new TableItem({
+          data: {id: "4"},
+          template: this.overflowMenuItemTemplate,
+        }), new TableItem({
+        data: {id: "4"},
+        template: this.overflowMenuItemTemplateRemove,
+      }),
+        new TableItem({
+          data: {id: "4"},
+          template: this.overflowMenuItemTemplateEdit,
+        }),
+      ],
+      [
+        new TableItem({data: "Content"}),
+        new TableItem({data: "Content"}),
+        new TableItem({data: "Content"}),
+        new TableItem({
+          data: {id: "5"},
+          template: this.overflowMenuItemTemplate,
+        }), new TableItem({
+        data: {id: "5"},
+        template: this.overflowMenuItemTemplateRemove,
+      }),
+        new TableItem({
+          data: {id: "5"},
+          template: this.overflowMenuItemTemplateEdit,
+        }),
+      ],
+      [
+        new TableItem({data: "Content"}),
+        new TableItem({data: "Content"}),
+        new TableItem({data: "Content"}),
+        new TableItem({
+          data: {id: "6"},
+          template: this.overflowMenuItemTemplate,
+        }), new TableItem({
+        data: {id: "6"},
+        template: this.overflowMenuItemTemplateRemove,
+      }),
+        new TableItem({
+          data: {id: "6"},
+          template: this.overflowMenuItemTemplateEdit,
+        }),
+      ],
+      [
+        new TableItem({data: "Content"}),
+        new TableItem({data: "Content"}),
+        new TableItem({data: "Content"}),
+        new TableItem({
+          data: {id: "7"},
+          template: this.overflowMenuItemTemplate,
+        }), new TableItem({
+        data: {id: "7"},
+        template: this.overflowMenuItemTemplateRemove,
+      }),
+        new TableItem({
+          data: {id: "7"},
+          template: this.overflowMenuItemTemplateEdit,
+        }),
+      ],
+      [
+        new TableItem({data: "Content"}),
+        new TableItem({data: "Content"}),
+        new TableItem({data: "Content"}),
+        new TableItem({
+          data: {id: "8"},
+          template: this.overflowMenuItemTemplate,
+        }), new TableItem({
+        data: {id: "8"},
+        template: this.overflowMenuItemTemplateRemove,
+      }),
+        new TableItem({
+          data: {id: "8"},
+          template: this.overflowMenuItemTemplateEdit,
+        }),
+      ],
+      [
+        new TableItem({data: "Content"}),
+        new TableItem({data: "Content"}),
+        new TableItem({data: "Content"}),
+        new TableItem({
+          data: {id: "9"},
+          template: this.overflowMenuItemTemplate,
+        }), new TableItem({
+        data: {id: "9"},
+        template: this.overflowMenuItemTemplateRemove,
+      }),
+        new TableItem({
+          data: {id: "9"},
+          template: this.overflowMenuItemTemplateEdit,
+        }),
+      ],
+      [
+        new TableItem({data: "Content"}),
+        new TableItem({data: "Content"}),
+        new TableItem({data: "Content"}),
+        new TableItem({
+          data: {id: "10"},
+          template: this.overflowMenuItemTemplate,
+        }), new TableItem({
+        data: {id: "10"},
+        template: this.overflowMenuItemTemplateRemove,
+      }),
+        new TableItem({
+          data: {id: "10"},
+          template: this.overflowMenuItemTemplateEdit,
+        }),
+      ],
+    ];
+    this.dataSet = dataset;
+    this.model.data = dataset;
+
+    this.model.header = [
+      new TableHeaderItem({data: "Id"}),
+      new TableHeaderItem({data: "Name"}),
+      new TableHeaderItem({data: "Describe"}),
+      new TableHeaderItem({data: "Add"}),
+      new TableHeaderItem({data: "Delete"}),
+      new TableHeaderItem({data: "Edit"}),
+    ];
+  }
+
+  deleteData() {
+    this.dataSet.splice(0, 1);
+  }
+
+  isActiveOpenCUD: boolean = false;
+  currentOpenRole = 1;
+
+  openCUD(role: number) {
+    this.currentOpenRole = role;
+    this.isActiveOpenCUD = true;
+  }
+
+  closeCUD() {
+    this.isActiveOpenCUD = false;
+  }
+}
--- src/app/pages/layout/tabs/user-management/user-management.component.ts
+++ src/app/pages/layout/tabs/user-management/user-management.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, Input, TemplateRef, ViewChild} from '@angular/core';
 import { SharedModule } from '../../../../shared/shared.module';
+import {IconService, TableHeaderItem, TableItem, TableModel} from "carbon-components-angular";
+import Filter20 from '@carbon/icons/es/filter/20';
 
 @Component({
   selector: 'app-user-management',
@@ -8,4 +10,207 @@ import { SharedModule } from '../../../../shared/shared.module';
   templateUrl: './user-management.component.html',
   styleUrl: './user-management.component.scss',
 })
-export class UserManagementComponent {}
+export class UserManagementComponent {
+  @Input() model = new TableModel();
+  disabled = false;
+  @Input() size = "md";
+  @Input() showSelectionColumn = false;
+  @Input() enableSingleSelect = false;
+  @Input() striped = false;
+  @Input() sortable = true;
+  @Input() isDataGrid = false;
+  @Input() noData = false;
+  @Input() stickyHeader = false;
+  @Input() skeleton = false;
+  @Input() ariaLabelledby = "table";
+  @Input() ariaDescribedby = "desc";
+
+  selectPage(page: number) {
+    this.model.currentPage = page;
+  }
+
+  @ViewChild("overflowMenuItemTemplateImg", {static: false})
+  protected overflowMenuItemTemplateImg: TemplateRef<any> | undefined;
+
+  @ViewChild("overflowMenuItemTemplate", {static: false})
+  protected overflowMenuItemTemplate: TemplateRef<any> | undefined;
+
+  @ViewChild("overflowMenuItemTemplateEdit", {static: false})
+  protected overflowMenuItemTemplateEdit: TemplateRef<any> | undefined;
+  @ViewChild("overflowMenuItemTemplateRemove", {static: false})
+  protected overflowMenuItemTemplateRemove: TemplateRef<any> | undefined;
+
+
+  constructor(protected iconService: IconService) {
+    this.iconService.registerAll([Filter20]);
+  }
+
+  ngOnInit(): void {
+    // this.iconService.registerAll([UserAvatar20]);
+  }
+
+  ngOnChanges(): void {
+
+  }
+
+
+  ngAfterViewInit() {
+    this.model.data = [
+      [
+        new TableItem({
+          data: {id: "1"},
+          template: this.overflowMenuItemTemplateImg,
+        }),
+        new TableItem({data: "Hoang Gia Khuong"}),
+        new TableItem({data: "Khuong Gia Gia"}),
+        new TableItem({data: "1"}),
+        new TableItem({data: "476k"}),
+        new TableItem({data: "1"}),
+
+        new TableItem({
+          data: {id: "1"},
+          template: this.overflowMenuItemTemplate,
+        }),
+        new TableItem({
+          data: {id: "1"},
+          template: this.overflowMenuItemTemplateEdit,
+        }),
+
+      ],
+      [
+        new TableItem({
+          data: {id: "2"},
+          template: this.overflowMenuItemTemplateImg,
+        }),
+        new TableItem({data: "Dau Minh Tan "}),
+        new TableItem({data: "Tan Minh Dau"}),
+        new TableItem({data: "2"}),
+        new TableItem({data: "667k"}),
+        new TableItem({data: "2"}),
+
+        new TableItem({
+          data: {id: "2"},
+          template: this.overflowMenuItemTemplate,
+        }),
+        new TableItem({
+          data: {id: "2"},
+          template: this.overflowMenuItemTemplateEdit,
+        }),
+
+      ],
+      [
+        new TableItem({
+          data: {id: "3"},
+          template: this.overflowMenuItemTemplateImg,
+        }),
+        new TableItem({data: "Luu Dinh Quang Vinh"}),
+        new TableItem({data: "Vinh Quang Dinh Luu"}),
+        new TableItem({data: "3"}),
+        new TableItem({data: "118k"}),
+        new TableItem({data: "3"}),
+
+        new TableItem({
+          data: {id: "3"},
+          template: this.overflowMenuItemTemplate,
+        }),
+        new TableItem({
+          data: {id: "3"},
+          template: this.overflowMenuItemTemplateEdit,
+        }),
+
+      ],
+      [
+        new TableItem({
+          data: {id: "4"},
+          template: this.overflowMenuItemTemplateImg,
+        }),
+        new TableItem({data: "Bui Thanh Huy"}),
+        new TableItem({data: "Huy Thanh Bui"}),
+        new TableItem({data: "4"}),
+        new TableItem({data: "208k"}),
+        new TableItem({data: "4"}),
+
+        new TableItem({
+          data: {id: "4"},
+          template: this.overflowMenuItemTemplate,
+        }),
+        new TableItem({
+          data: {id: "4"},
+          template: this.overflowMenuItemTemplateEdit,
+        }),
+
+      ],
+      [
+        new TableItem({
+          data: {id: "5"},
+          template: this.overflowMenuItemTemplateImg,
+        }),
+        new TableItem({data: "Nguyen Tuan Anh"}),
+        new TableItem({data: "Tuan Anh Nguyen"}),
+        new TableItem({data: "5"}),
+        new TableItem({data: "200k"}),
+        new TableItem({data: "5"}),
+
+        new TableItem({
+          data: {id: "5"},
+          template: this.overflowMenuItemTemplate,
+        }),
+        new TableItem({
+          data: {id: "5"},
+          template: this.overflowMenuItemTemplateEdit,
+        }),
+
+
+      ],
+      [
+        new TableItem({
+          data: {id: "6"},
+          template: this.overflowMenuItemTemplateImg,
+        }),
+        new TableItem({data: "Bui Quang Truong"}),
+        new TableItem({data: "Truong Bui Quang "}),
+        new TableItem({data: "6"}),
+        new TableItem({data: "300k"}),
+        new TableItem({data: "6"}),
+        new TableItem({
+          data: {id: "6"},
+          template: this.overflowMenuItemTemplate,
+        }),
+        new TableItem({
+          data: {id: "6"},
+          template: this.overflowMenuItemTemplateEdit,
+        }),
+      ],
+      [
+        new TableItem({
+          data: {id: "6"},
+          template: this.overflowMenuItemTemplateImg,
+        }),
+        new TableItem({data: "Bui Quang Truong"}),
+        new TableItem({data: "Truong Bui Quang "}),
+        new TableItem({data: "6"}),
+        new TableItem({data: "300k"}),
+        new TableItem({data: "6"}),
+        new TableItem({
+          data: {id: "6"},
+          template: this.overflowMenuItemTemplate,
+        }),
+        new TableItem({
+          data: {id: "6"},
+          template: this.overflowMenuItemTemplateEdit,
+        }),
+      ],
+    ];
+    this.model.header = [
+      new TableHeaderItem({data: "Image"}),
+      new TableHeaderItem({data: "UserName"}),
+      new TableHeaderItem({data: "NickName"}),
+      new TableHeaderItem({data: "NumberPost"}),
+      new TableHeaderItem({data: "Followers"}),
+      new TableHeaderItem({data: "Following"}),
+      new TableHeaderItem({data: "Block"}),
+      new TableHeaderItem({data: "Unblock"}),
+    ];
+  }
+
+}
