b861a4c7f89fe47496bcf1168004dbecc1beddf4
hien.hv6193 <hien.hv6193@sinhvien.hoasen.edu.vn>
2024-03-08 10:21:48 +0700

    role select

--- src/app/ngrx/role/role.effect.ts
+++ src/app/ngrx/role/role.effect.ts
@@ -11,24 +11,32 @@ export class RoleEffect {
 
   $getAllRole = createEffect(() => this.actions.pipe(
     ofType(RoleActions.getAllRole),
-    switchMap((action) => this.roleService.getAllRole().pipe(
-      map((role: any) => RoleActions.getAllRoleSuccess({role: role})),
+    switchMap((action) => this.roleService.getAllRole(action.token,action.page).pipe(
+      map((roles: any) => RoleActions.getAllRoleSuccess({roles})),
       catchError((error) => [RoleActions.getAllRoleFailure({error})])
+    ))
+  ));
+
+  $getAllSearchRole = createEffect(() => this.actions.pipe(
+    ofType(RoleActions.getAllSearchRole),
+    switchMap((action) => this.roleService.getAllSearchRole(action.token,action.page).pipe(
+      map((roles: any) => RoleActions.getAllSearchRoleSuccess({roles})),
+      catchError((error) => [RoleActions.getAllSearchRoleFailure({error})])
     ))
   ));
 
   $createRole = createEffect(() => this.actions.pipe(
     ofType(RoleActions.createRole),
     switchMap((action) => this.roleService.createRole(action.role).pipe(
-      map((role: any) => RoleActions.createRoleSuccess({role: role})),
+      map((role: any) => RoleActions.createRoleSuccess({roles: role})),
       catchError((error) => [RoleActions.createRoleFailure({error})])
     ))
   ));
 
   $updateRole = createEffect(() => this.actions.pipe(
     ofType(RoleActions.updateRole),
-    switchMap((action) => this.roleService.updateRole(action.role).pipe(
-      map((role: any) => RoleActions.updateRoleSuccess({role: role})),
+    switchMap((action) => this.roleService.updateRole(action.role.id, action.role).pipe(
+      map((role: any) => RoleActions.updateRoleSuccess({roles: role})),
       catchError((error) => [RoleActions.updateRoleFailure({error})])
     ))
   ));
