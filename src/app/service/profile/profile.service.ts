6668edcc5841cf79682fa65c794b99f8144741a9
Vo Minh Tri <vominhtri1610@gmail.com>
2024-03-11 09:58:13 +0700

    update dashboard

--- src/app/service/profile/profile.service.ts
+++ src/app/service/profile/profile.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpHeaders} from "@angular/common/http";
-import {environment} from "../../../environments/environment";
-import {ProfileModel} from "../../models/profile.model";
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { ProfileModel } from '../../models/profile.model';
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProfileService {
   constructor(private httpClient: HttpClient) {}
@@ -16,9 +17,18 @@ export class ProfileService {
         headers: new HttpHeaders({
           Authorization: ` ${idToken}`,
         }),
-      }
+      },
     );
   }
+
+  getProfileById(token: string, id: string) {
+    return this.httpClient.get<ProfileModel>(
+      environment.local_url + `profile?id=${id}`,
+      {
+        headers: new HttpHeaders({
+          Authorization: ` ${token}`,
+        }),
+      },
+    );
   }
-
-
+}
