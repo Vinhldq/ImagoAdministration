9c68469c4880637bdd6cf540eb94952b9e457b0d
DucDat <dat.hd3745@sinhvien.hoasen.edu.vn>
2024-03-14 12:26:08 +0700

    pull

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -49,19 +49,19 @@ export const appConfig: ApplicationConfig = {
     ]),
     provideState({ name: 'report', reducer: reportReducer }),
     provideHttpClient(),
-    // importProvidersFrom(
-    //   provideFirebaseApp(() =>
-    //     initializeApp({
-    //       projectId: 'itss-imago-0000',
-    //       appId: '1:1098187958856:web:931b5d503852e1c9a1867d',
-    //       storageBucket: 'itss-imago-0000.appspot.com',
-    //       apiKey: 'AIzaSyAJ93BuFGs7gOJe9kudLYvAn4-Fp6Q936M',
-    //       authDomain: 'itss-imago-0000.firebaseapp.com',
-    //       messagingSenderId: '1098187958856',
-    //       measurementId: 'G-7TVCQGP8RS',
-    //     }),
-    //   ),
-    // ),
+    importProvidersFrom(
+      provideFirebaseApp(() =>
+        initializeApp({
+          projectId: 'itss-imago-0000',
+          appId: '1:1098187958856:web:931b5d503852e1c9a1867d',
+          storageBucket: 'itss-imago-0000.appspot.com',
+          apiKey: 'AIzaSyAJ93BuFGs7gOJe9kudLYvAn4-Fp6Q936M',
+          authDomain: 'itss-imago-0000.firebaseapp.com',
+          messagingSenderId: '1098187958856',
+          measurementId: 'G-7TVCQGP8RS',
+        })
+      )
+    ),
     // importProvidersFrom(
     //   provideFirebaseApp(() =>
     //     initializeApp({
@@ -76,19 +76,19 @@ export const appConfig: ApplicationConfig = {
     //   )
     // ),
 
-    importProvidersFrom(
-      provideFirebaseApp(() =>
-        initializeApp({
-          projectId: 'imago-backup2',
-          appId: '1:307613940415:web:590dc59592b2b83788240a',
-          storageBucket: 'imago-backup2.appspot.com',
-          // locationId: 'asia-east1',
-          apiKey: 'AIzaSyDvKdVzEJmlsJtk4ugeFc_yY-hEQ1RK6-o',
-          authDomain: 'imago-backup2.firebaseapp.com',
-          messagingSenderId: '307613940415',
-        })
-      )
-    ),
+    // importProvidersFrom(
+    //   provideFirebaseApp(() =>
+    //     initializeApp({
+    //       projectId: 'imago-backup2',
+    //       appId: '1:307613940415:web:590dc59592b2b83788240a',
+    //       storageBucket: 'imago-backup2.appspot.com',
+    //       // locationId: 'asia-east1',
+    //       apiKey: 'AIzaSyDvKdVzEJmlsJtk4ugeFc_yY-hEQ1RK6-o',
+    //       authDomain: 'imago-backup2.firebaseapp.com',
+    //       messagingSenderId: '307613940415',
+    //     })
+    //   )
+    // ),
     importProvidersFrom(provideAuth(() => getAuth())),
     importProvidersFrom(provideFirestore(() => getFirestore())),
     importProvidersFrom(provideStorage(() => getStorage())),
---
+++ src/app/shared/pipes/posts/IdToPicture/id-to-picture.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { IdToPicturePipe } from './id-to-picture.pipe';
+
+describe('IdToPicturePipe', () => {
+  it('create an instance', () => {
+    const pipe = new IdToPicturePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
